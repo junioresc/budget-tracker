ea5d673cd0e3b90393517401a08d28b8daacfb43
Junioresc <junioresc1092@gmail.com>
2020-10-10 09:31:26 -0400

    updated readme and cached the icons

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # budget-tracker
+
+[![License](https://img.shields.io/badge/Licensed%20by-MIT%20License-blue)](LICENSE.txt)
+
+## Description
+
+Have you ever gone out and then the next thing you know it, you ran out of money because you thought, $5 here wouldn't hurt and another $10 here buying boba tea sounds like pennies but then come to find out at the end of the day that you have no more money left in the bank? This budget tracker could be the application to solve your problems. Inputting every single thing into the app will help you keep track of your money to prevent overspending. There is even a nice graph to help visualize your spending or if you like seeing the graph go up, your frugal savings. If anything, hopefully this application can help you make conscious and finanically responsible decisions.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+When you go onto the site, there will be a little plus sign inside of a circle on the right of the url bar. Once you click it and hit install you'll have a shortcut on your desktop to be able to use the application. You can also install it on your phone if you would like.
+
+## Usage
+
+### Link to the application
+
+https://mysterious-coast-52354.herokuapp.com/
+
+Once you are on the site, you will be able to track your budget from anywhere. It will even work offline and keep the data updated when you finally gain internet connection again. Just type the name of the transaction, the cost and click if it is a deposit or a withdrawal of money.
+
+![IMAGE ALT TEXT](https://cdn.discordapp.com/attachments/749986989021069372/764473193956114472/unknown.png)
+
+## License
+
+Licensed under the [MIT License](LICENSE.txt).
+
+## Questions
+
+My [GitHub](https://github.com/junioresc/)  
+If you have any additional questions and would like to reach me, you can at junioresc1092@gmail.com
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,51 +1,60 @@
 const FILES_TO_CACHE = [
-    `./index.html`,
-    `./css/styles.css`,
-    `./js/idb.js`,
-    `./js/index.js`
+  `./index.html`,
+  `./css/styles.css`,
+  `./js/idb.js`,
+  `./js/index.js`,
+  `./icons/icon-72x72`,
+  `./icons/icon-96x96`,
+  `./icons/icon-128x128`,
+  `./icons/icon-144x144`,
+  `./icons/icon-152x152`,
+  `./icons/icon-384x384`,
+  `./icons/icon-512x512`,
 ];
 
 const APP_PREFIX = `BudgetTracker-`;
 const VERSION = `version_01`;
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-self.addEventListener(`install`, function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log(`installing cache : ${CACHE_NAME}`)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
+self.addEventListener(`install`, function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log(`installing cache : ${CACHE_NAME}`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener(`activate`, function(event) {
-    event.waitUntil(
-        caches.keys().then(function(keyList) {
-            let cacheKeeplist = keyList.filter(function(key) {
-                return key.indexOf(APP_PREFIX);
-            })
-            cacheKeeplist.push(CACHE_NAME);
-            return Promise.all(keyList.map(function(key, i) {
-                if(cacheKeeplist.indexOf(key) === -1) {
-                    console.log(`deleting cache : ${keyList[i]}`);
-                    return caches.delete(keyList[i]);
-                }
-            }));
+self.addEventListener(`activate`, function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log(`deleting cache : ${keyList[i]}`);
+            return caches.delete(keyList[i]);
+          }
         })
-    );
+      );
+    })
+  );
 });
 
-self.addEventListener(`fetch`, function(event) {
-    console.log(`fetch request : ${event.request.url}`)
-    event.respondWith(
-        caches.match(event.request).then(function(request) {
-            if(request) {
-                console.log(`responding with cache : ${event.request.url}`)
-                return request
-            } else {
-                console.log(`file is not cached, fetching : ${event.request.url}`)
-                return fetch(event.request)
-            }
-        })
-    )
+self.addEventListener(`fetch`, function (event) {
+  console.log(`fetch request : ${event.request.url}`);
+  event.respondWith(
+    caches.match(event.request).then(function (request) {
+      if (request) {
+        console.log(`responding with cache : ${event.request.url}`);
+        return request;
+      } else {
+        console.log(`file is not cached, fetching : ${event.request.url}`);
+        return fetch(event.request);
+      }
+    })
+  );
 });
